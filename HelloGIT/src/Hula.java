8f29bae9a3ee7179644be0c153f5f3abd96b9f40
ThaiDuong <ThaiDuong@Duong.libelle.ag>
2014-04-07 21:13:53 +0200

    Create new Hula.java class

---
+++ HelloGIT/src/Hula.java
@@ -0,0 +1,14 @@
+public class Hula {
+
+	public Hula() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test Hula");
+	}
+
+}
